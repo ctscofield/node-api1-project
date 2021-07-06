5d47a8e754d81bb7fb1567951429b31b5e809d7e
ctscofield <ctscofield@gmail.com>
2021-07-05 18:57:52 -0600

    delete enpoint

--- api/server.js
+++ api/server.js
@@ -73,4 +73,20 @@ server.put("/api/users/:id", (req, res) => {
             })
     }
 })
+
+server.delete("/api/users/:id", async (req, res) => {
+    try {
+        const deleted = await users.remove(req.params.id);
+        if (!deleted) {
+            res.status(404).json({message: "The user with the specified ID does not exist"});
+        } else {
+            res.json(deleted);   
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
