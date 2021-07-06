4a63e194b951aa3bd00db14ff8b428231c7bd59b
ctscofield <ctscofield@gmail.com>
2021-07-05 18:39:47 -0600

    put endpoint

--- api/server.js
+++ api/server.js
@@ -51,5 +51,26 @@ server.post("/api/users", (req, res) => {
     }
 });
 
-
+server.put("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, bio } = req.body;
+    if (!name || !bio) {
+        res.status(400).json({message: `Please provide name and bio for the user`})
+    } else {
+        users.update(id, { name, bio})
+            .then(updated => {
+                if (!updated) {
+                    res.status(404).json({ message: `The user with the specified ID does not exist`});
+                } else {
+                    res.status(200).json(updated);
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: `The user information could not be modified`,
+                    error: err.message
+                })
+            })
+    }
+})
 module.exports = server; // EXPORT YOUR SERVER instead of {}
